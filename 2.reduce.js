8e707c73e164b443bae4d8c9a46d49a1c398e45f
gergokutu <gergo.kutu@gmail.com>
2019-07-11 18:18:45 +0200

    cleaned up calculateAverageSpawnChance

--- 2.reduce.js
+++ 2.reduce.js
@@ -4,23 +4,36 @@ const calculateTotalPokemonWeight = (pokemons) => {
     }, 0)
 }
 
+// const calculateAverageSpawnChance = (pokemons) => {
+//     // console.log('POKEMONS:', pokemons)
+//     // 1. add up all the spawn_chances of the pokemon
+//     const totalSpawnChance = pokemons.reduce((accumulator, currentValue) => {
+//         // console.log('inside totalSpawnChances')
+//         // console.log('accumulator:', accumulator)
+//         // console.log('currentValue:', currentValue.spawn_chance)
+//         return accumulator + currentValue.spawn_chance
+//     }, 0)
+//     // 2. divide by the number of pokemon
+//     const averageSpawnChance = totalSpawnChance / pokemons.length 
+//     // 3. return the value from the screen
+//     // console.log(averageSpawnChance)
+//     return averageSpawnChance
+// }
+
+// clean it up!!!
+// delete comments and console.log
 const calculateAverageSpawnChance = (pokemons) => {
-    // console.log('POKEMONS:', pokemons)
-    // 1. add up all the spawn_chances of the pokemon
     const totalSpawnChance = pokemons.reduce((accumulator, currentValue) => {
-        // console.log('inside totalSpawnChances')
-        // console.log('accumulator:', accumulator)
-        // console.log('currentValue:', currentValue.spawn_chance)
         return accumulator + currentValue.spawn_chance
     }, 0)
-    // 2. divide by the number of pokemon
-    const averageSpawnChance = totalSpawnChance / pokemons.length 
-    // 3. return the value from the screen
-    // console.log(averageSpawnChance)
-    return averageSpawnChance
+    // next two line in one line
+    // const averageSpawnChance = totalSpawnChance / pokemons.length
+    // return averageSpawnChance
+    return totalSpawnChance / pokemons.length
 }
 
 module.exports = {
     calculateTotalPokemonWeight,
     calculateAverageSpawnChance
+    // calculateTotalEggDistance
 }
